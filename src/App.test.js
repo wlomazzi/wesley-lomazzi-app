4787dcc395ad384622100b78bc07dd78f2e712fa
wlomazzi <wlomazzi@gmail.com>
2026-04-08 08:22:57 -0600

    Added unit test to check my name on the page

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders my name on the page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const nameElement = screen.getByText(/Wesley Lomazzi/i);
+  expect(nameElement).toBeInTheDocument();
 });
